be487e0e41b5d1e2796617beaeb0c82c311127e2
Mohamed <lm_rebiai@esi.dz>
2024-02-02 23:22:25 +0100

    test sign up

--- test.py
+++ test.py
@@ -1,31 +1,53 @@
-from selenium import webdriver
 import time
+from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
-driver = webdriver.Chrome()
 
-website = 'http://localhost:5173'
+def test_login(driver, username, password, website):
+    driver.get(f"{website}/login")
+    driver.maximize_window()
+    driver.find_element(By.ID, 'name').send_keys(username)
+    driver.find_element(By.ID, 'password').send_keys(password)
+    driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div/div/div/button').click()
 
-# testing the login page
-driver.get('/'.join([website, 'login']))
-driver.maximize_window()
+def test_signup(driver, username, email, password, website):
+    driver.get(f"{website}/signup")
+    driver.maximize_window()
 
-username = "yahiaa05"
-password = "AdminAdmin123@"
-driver.find_element(by=By.XPATH, value='//*[@id="name"]').send_keys(username)
-driver.find_element(by=By.XPATH, value='//*[@id="password"]').send_keys(password)
-driver.find_element(by=By.XPATH, value='//*[@id="root"]/div/div/div/div/div/button').click()
+    driver.find_element(By.ID, 'username').clear()
+    driver.find_element(By.ID, 'username').send_keys(username)
 
-# testing the signup page
+    driver.find_element(By.ID, 'email').clear()
+    driver.find_element(By.ID, 'email').send_keys(email)
 
+    driver.find_element(By.ID, 'password').clear()
+    driver.find_element(By.ID, 'password').send_keys(password)
 
-username = "mohamed"
-password = "12345678"
-email = 'ab@gmail.com'
+    driver.find_element(By.ID, 'confirmPassword').clear()
+    driver.find_element(By.ID, 'confirmPassword').send_keys(password)
 
+    WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div/div/div/div/div/button'))).click()
 
-time.sleep(5)
-driver.quit()
+def main():
+    driver = webdriver.Edge()
+    website = 'http://localhost:5173'
+    choice = input("Enter 1 to test login or 2 to test signup: ")
+    if choice == "1":
+        username = "yahiaa05"
+        password = "AdminAdmin123@"
+        test_login(driver, username, password, website)
+        time.sleep(5)
+    elif choice == "2":
+        username = 'testuser'
+        email = 'ab@gmail.com'
+        password = 'password123'
+        test_signup(driver, username, email, password, website)
+        time.sleep(5)
+    else:
+        print("Invalid choice")
+
+    driver.quit()
+
+if __name__ == "__main__":
+    main()
