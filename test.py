7bba63b561602885b51ed560edd3e82376108a01
Mohamed <lm_rebiai@esi.dz>
2024-02-03 21:11:18 +0100

    add to fav test added

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 
 def test_login(driver, username, password, website):
     driver.get(f"{website}/login")
@@ -29,10 +31,38 @@ def test_signup(driver, username, email, password, website):
 
     WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div/div/div/div/div/button'))).click()
 
+def add_to_favorite(driver, website):
+    test_login(driver, "admin", "123456", website)
+    WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div/div/div/button'))).click()
+    WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div/ul/li[2]/button'))).click()
+
+    # Wait for the alert to be present
+    alert = WebDriverWait(driver, 5).until(EC.alert_is_present())
+
+    # Get the text of the alert
+    alert_text = alert.text
+
+    # Verify the text of the alert
+    if alert_text == "Added to favorites!":
+        print("it works!")
+    else:
+        print("Alert text does not match")
+
+    # Accept the alert
+    alert.accept()
+
+
+
+def add_to_menu():
+    print("Enter 1 to test login")
+    print("Enter 2 to test signup")
+    print("Enter 3 to add to favorite")
+
 def main():
     driver = webdriver.Edge()
     website = 'http://localhost:5173'
-    choice = input("Enter 1 to test login or 2 to test signup: ")
+    choice = '3'
+    # input("Enter your choice: ")
     if choice == "1":
         username = "yahiaa05"
         password = "AdminAdmin123@"
@@ -43,6 +73,9 @@ def main():
         email = 'ab@gmail.com'
         password = 'password123'
         test_signup(driver, username, email, password, website)
+        time.sleep(5)
+    elif choice == "3":
+        add_to_favorite(driver, website)
         time.sleep(5)
     else:
         print("Invalid choice")
